db52a7e6f1f1ae157244d4564428742d4d703cac
Amaury DOUBLET <amaury.doublet@viacesi.fr>
2024-12-07 11:30:41 +0100

    commentaire plus fonction

--- GraphicGame.h
+++ GraphicGame.h
@@ -1,11 +1,11 @@
-#ifndef GAME_HPP // Protection contre les inclusions multiples.
-#define GAME_HPP // Définition de la macro GAME_HPP pour éviter les inclusions multiples.
+#ifndef GAME_HPP 
+#define GAME_HPP 
 
-#include <SFML/Graphics.hpp> // Inclusion de la bibliothèque SFML pour les graphiques.
-#include <iostream> // Inclusion de la bibliothèque pour les entrées/sorties standard.
-#include <fstream> // Inclusion pour la gestion des fichiers.
-#include <vector> // Inclusion pour utiliser les vecteurs.
-#include "Grid.h" // Inclusion de la classe Grid (grille).
+#include <SFML/Graphics.hpp> 
+#include <iostream> 
+#include <fstream> 
+#include <vector> 
+#include "Grid.h" 
 
 struct StructureData { // Structure pour représenter les données à charger.
     std::vector<std::pair<int, int>> cells; // Liste de cellules à activer.
@@ -13,15 +13,16 @@ struct StructureData { // Structure pour représenter les données à charger.
 
 class GraphicGame {
 private:
-    Grid grid; // Instance de la grille pour le jeu.
+    Grid grid; 
     sf::RenderWindow window; // Fenêtre de rendu graphique SFML.
     sf::Font font; // Police utilisée pour afficher du texte.
-    sf::Text iterationText; // Texte affichant le nombre d'itérations.
+    sf::Text iterationText; 
     sf::Text touche;
     sf::Text touche2;
+    sf::Text toucheT;
 
     bool running; // Indique si la simulation est en cours.
-    bool editing; // Mode édition pour permettre de modifier les cellules avec la souris.
+    bool editing; // Mode édition.
     int iterationCount; // Compteur du nombre d'itérations.
     int delay; // Délai entre les mises à jour de la grille (en millisecondes).
     StructureData structureData; // Variable pour stocker les données de la structure.
@@ -29,7 +30,7 @@ private:
 public:
     GraphicGame(int ligne, int colonne, float Size, int delayMs) // Constructeur avec paramètres pour la grille.
         : grid(ligne, colonne, Size), // Initialisation de la grille.
-        window(sf::VideoMode(colonne* Size, ligne* Size + 60), "Jeu de la Vie"), // Initialisation de la fenêtre.
+        window(sf::VideoMode(colonne* Size, ligne* Size + 100), "Jeu de la Vie"), // Initialisation de la fenêtre.
         running(false), editing(true), iterationCount(0), delay(delayMs) { // Initialisation des variables.
 
         if (!font.loadFromFile("Roboto-Regular.ttf")) { // Chargement de la police.
@@ -39,7 +40,13 @@ public:
         iterationText.setFont(font); // Configuration de la police pour le texte.
         iterationText.setCharacterSize(20); // Taille du texte.
         iterationText.setFillColor(sf::Color::Black); // Couleur du texte.
-        iterationText.setPosition(10, ligne * Size + 10); // Position du texte sous la grille.
+        iterationText.setPosition(5, ligne * Size + 10); // Position du texte sous la grille.
+
+        //la même chose pour la touche T
+        toucheT.setFont(font); 
+        toucheT.setCharacterSize(20); 
+        toucheT.setFillColor(sf::Color::Black); 
+        toucheT.setPosition(5, ligne * Size + 40); 
     }
 
     GraphicGame(const std::string& filename, float Size, int delayMS) // Constructeur pour charger une grille depuis un fichier.
@@ -48,16 +55,21 @@ public:
         running(false), editing(false), iterationCount(0), delay(delayMS) { // Initialisation des variables.
 
         grid.loadFromFile(filename); // Chargement de la grille depuis un fichier.
-        window.create(sf::VideoMode(grid.getcolonne() * Size, grid.getligne() * Size + 60), "Jeu de la Vie"); // Redimensionnement de la fenêtre selon la grille.
+        window.create(sf::VideoMode(grid.getcolonne() * Size  , grid.getligne() * Size + 100), "Jeu de la Vie"); // Redimensionnement de la fenêtre selon la grille.
         
         if (!font.loadFromFile("Roboto-Regular.ttf")) { // Chargement de la police.
             throw std::runtime_error("Impossible de charger la police 'Roboto-Regular.ttf'."); // Erreur si la police n'est pas chargée.
         }
 
-        iterationText.setFont(font); // Configuration de la police pour le texte.
-        iterationText.setCharacterSize(20); // Taille du texte.
-        iterationText.setFillColor(sf::Color::Black); // Couleur du texte.
-        iterationText.setPosition(10, grid.getligne() * Size + 10); // Position du texte sous la grille.
+        iterationText.setFont(font); 
+        iterationText.setCharacterSize(20); 
+        iterationText.setFillColor(sf::Color::Black); 
+        iterationText.setPosition(5, grid.getligne() * Size + 10); 
+
+        toucheT.setFont(font); 
+        toucheT.setCharacterSize(20); 
+        toucheT.setFillColor(sf::Color::Black); 
+        toucheT.setPosition(5, grid.getligne() * Size + 40);
 
     
     }
@@ -152,26 +164,27 @@ public:
                 }
             }
         }
-        else if (event.type == sf::Event::KeyPressed) {
+        // fonctionnement des touches
+        else if (event.type == sf::Event::KeyPressed) { // utilisation de P pour le départ ou l'arret
             if (event.key.code == sf::Keyboard::P) {
                 running = !running;
                 editing = !running;
             }
-            else if (event.key.code == sf::Keyboard::G && !running) {
+            else if (event.key.code == sf::Keyboard::G && !running) { // utilisation de G pour créer un glider
                 loadStructureFromFile("glider.txt");
                 std::cout << "Structure chargée. Cliquez avec le bouton droit pour l'appliquer." << std::endl;
             }
-            else if (event.key.code == sf::Keyboard::H && !running) {
+            else if (event.key.code == sf::Keyboard::H && !running) { // utilisation de H pour créer un canon a glider
                 loadStructureFromFile("canon_glider.txt");
                 std::cout << "Structure chargée. Cliquez avec le bouton droit pour l'appliquer." << std::endl;
             }
-            else if (event.key.code == sf::Keyboard::C && editing) {
+            else if (event.key.code == sf::Keyboard::C && editing) { //utilisation de C pour fermer la fenêtre 
                 window.close();
             }
-            else if (event.key.code == sf::Keyboard::R && editing) {
+            else if (event.key.code == sf::Keyboard::R && editing) { //utilisation de R pour reset la page
                 grid.clearGrid();
             }
-            else if (event.key.code == sf::Keyboard::T) {
+            else if (event.key.code == sf::Keyboard::T) { // utilisation de T pour afficher le menu
                 std::string message = "Pause : P\n"
                     "Reinitialiser : R      \n"
                     "Objet 1 : G\n"
@@ -202,10 +215,12 @@ public:
             touche.setString("Pause : P    Reinitialiser : R, Objet 1 : G   Objet 2 : H    Fermer : C");
 
             iterationText.setString("Iterations: " + std::to_string(iterationCount)); // Mise à jour du texte d'itérations.
+            toucheT.setString("menu: T ");//affichage T
 
             window.clear(sf::Color::White); // Efface la fenêtre avec un fond blanc.
             grid.draw(window); // Dessine la grille.
             window.draw(iterationText); // Dessine le texte des itérations.
+            window.draw(toucheT); //dessiner touche T
             window.draw(touche); // Dessine le texte des instructions.
             window.display(); // Affiche le contenu de la fenêtre.
         }
